b956e132a18925692720cd9aca8f4bae0c1d504a
jacknorthrup <jack.northrup.ph@gmail.com>
2020-01-12 15:46:58 +0800

    edit

---
+++ Completedpy.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from time import sleep
+import glob
+def track_download():
+    list_of_files = glob.glob('/home/jack/Downloads/*') # * means all if need specific format then *.csv
+    src = max(list_of_files, key=os.path.getctime)
+    TEXT=[]
+    while True:
+        file1 = os.stat(src) # initial file size
+        file1_size = file1.st_size
+        num=file1_size*.000001
+        sys.stdout.write("%.2f" % num)
+        sys.stdout.write("Mb ")
+        #sys.stdout.write(file1_size),"Mb  ")
+        sys.stdout.flush()
+        # your script here that collects and writes data (increase file size)
+        #print("%.2f" % file1_size),"Mb  ",
+        sleep(10)
+        file2 = os.stat(src) # updated file size
+        
+        file2_size = file2.st_size
+        comp = file2_size - file1_size # compares sizes
+        if comp == 0:
+            #restart_program(src)
+            TEXT.append("The File Appears to be completed.")
+            print ("No filesize change")
+            break
+        else:
+            sleep(10)
+    return TEXT
---
+++ watchVID.py
@@ -0,0 +1,15 @@
+import subprocess
+import glob
+import os
+def WATCH():
+    # detox: remove all spaces from filenames Linux: apt install detox
+    #PYTHON3 use: subprocess.run(["detox", "/home/jack/Downloads"])
+    THIS_dir os.getcwd()
+    subprocess.call(["detox", THIS_dir])
+    list_of_files = glob.glob(THIS_dir+"/*.mp4") # * means all if need specific format then *.csv
+    src = max(list_of_files, key=os.path.getctime)
+    bashCommand ="vlc --preferred-resolution 240 "+src 
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate() 
+    return 
+
